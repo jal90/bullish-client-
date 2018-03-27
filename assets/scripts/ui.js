57031f2c8fb76673addf8d11461d06ae43b1e980
mxavier91 <mxavier17@gmail.com>
2018-03-27 10:18:01 -0400

    Fields clear after submit and hid not functional buttons
    Used JQuery and CSS to hide no functional buttons.
    
    Members: Louis

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -7,7 +7,7 @@ const signUpSuccess = function (data) {
   $('#email-field').val('')
   $('#password-field').val('')
   $('#password-conformation-field').val('')
-  // $('#sign-up').hide()
+  $('#sign-up').hide()
 }
 
 const signUpFailure = function (error) {
@@ -25,15 +25,15 @@ const signInSuccess = function (data) {
   $('#message').css('background-color', 'green')
   $('#signInEmail').val('')
   $('#signInPassword').val('')
-  // $('#change-password').show()
-  // $('#sign-out').show()
-  // $('#create').show()
-  // $('#showAll').show()
-  // $('#update').show()
-  // $('#delete').show()
-  // $('#all-users').show()
-  // $('#sign-up').hide()
-  // $('#sign-in').hide()
+  $('#change-password').show()
+  $('#sign-out').show()
+  $('#create').show()
+  $('#showAll').show()
+  $('#update').show()
+  $('#delete').show()
+  $('#all-users').show()
+  $('#sign-up').hide()
+  $('#sign-in').hide()
   store.user = data.user
 }
 
@@ -63,15 +63,15 @@ const changeFailure = function (error) {
 const signOutSuccess = function () {
   $('#message').text('"I\'ll be Back"-Signed out Successfully')
   $('#message').css('background-color', 'green')
-  // $('#change-password').hide()
-  // $('#sign-out').hide()
-  // // $('#create').hide()
-  // // $('#showAll').hide()
-  // // $('#update').hide()
-  // // $('#delete').hide()
-  // // $('#content').hide()
-  // $('#sign-up').show()
-  // $('#sign-in').show()
+  $('#change-password').hide()
+  $('#sign-out').hide()
+  $('#create').hide()
+  $('#showAll').hide()
+  $('#update').hide()
+  $('#delete').hide()
+  $('#content').hide()
+  $('#sign-up').show()
+  $('#sign-in').show()
 }
 
 const signOutFailure = function (error) {
@@ -84,6 +84,47 @@ const showAllSuccess = function (data) {
   $('body').append(JSON.stringify(data))
 }
 
+const createSuccessful = function (data) {
+  console.log(data)
+  $('#message').text('"It\'s Alive, It\'s Alive!!!"-New Movie Added')
+  $('#message').css('background-color', 'green')
+  $('#createSurvey').val('')
+  store.movie = data.movie
+}
+
+const createFailed = function (data) {
+  $('#message').text('"Argo F@%# Yourself"-Error Adding Movie')
+  $('#message').css('background-color', 'red')
+  $('#createSurvey').val('')
+}
+
+const updateSuccess = function (data) {
+  $('#message').text('"I wish I knew how to quit you"-Successfully Update')
+  $('#message').css('background-color', 'green')
+  $('#updateId').val('')
+  $('#updateSurvey').val('')
+}
+
+const updateFailed = function (data) {
+  $('#message').text('Hold up one sec, Update Failure')
+  $('#message').css('background-color', 'red')
+  $('#updateId').val('')
+  $('#updateSurvey').val('')
+}
+
+const deleteSuccess = function () {
+  $('#message').text('"Leave the gun. Take the Cannoli"-Delete Successfully')
+  $('#message').css('background-color', 'green')
+  $('#deleteSurvey').val('')
+  console.log('deleteSuccessful')
+}
+
+const deleteFailed = function () {
+  $('#message').text('Hold up one sec, Failed to Delete')
+  $('#message').css('background-color', 'red')
+  $('#deleteSurvey').val('')
+}
+
 module.exports = {
   signUpSuccess,
   signUpFailure,
@@ -93,5 +134,11 @@ module.exports = {
   changeFailure,
   signOutSuccess,
   signOutFailure,
-  showAllSuccess
+  createSuccessful,
+  createFailed,
+  showAllSuccess,
+  updateSuccess,
+  updateFailed,
+  deleteSuccess,
+  deleteFailed
 }
